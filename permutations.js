e98429a57af4f3392434a1ab47c8405b791d9b70
Gerald McAuley <geraldmcauley@gmail.com>
2017-12-19 16:16:31 +0000

    Add permutations script to repo

---
+++ permutations.js
@@ -0,0 +1,14 @@
+function permAlone(str) {
+  return str;
+}
+
+permAlone('aab');
+
+
+
+//how many different arrangemnets of the string letters can be made without repeating any letter
+
+//create a regular expression that returns strings of length of original string
+//letter 1 - can be anything
+//2- cannot be the  previous letter
+//3-- etc
