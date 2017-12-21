fd06646c3edb0a4a7fdc6af1cf37720e150723ec
Gerald McAuley <geraldmcauley@gmail.com>
2017-12-21 14:58:56 +0000

    Map debris code

---
+++ mapTheDebris.js
@@ -0,0 +1,17 @@
+function orbitalPeriod(arr) {
+  var GM = 398600.4418;
+  var earthRadius = 6367.4447;
+  var avgAlt =0;
+
+
+  var a =0;
+  var b = Math.pow(earthRadius);
+  var c =0;
+
+console.log(arr[0].name);
+
+
+  return arr;
+}
+
+orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]);
