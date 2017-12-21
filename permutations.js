1477405ac524bf0bcab787a7330ac3e8f7cec6ba
Gerald McAuley <geraldmcauley@gmail.com>
2017-12-21 10:21:04 +0000

    Add working permutation script

--- permutations.js
+++ permutations.js
@@ -1,9 +1,14 @@
-//get all permutations
+///get all permutations
 //use regexp to remove string with repeating charters
 
 
-//cycle through array to create permutations
-var permArr = [], usedChars = [];
+function permAlone(str){
+    var permArr = [], usedChars = [];
+    //REGULAR EXPRESSION TO MATCH REPEATING CHARACTER IN A STRING
+    var reg = /(\w)\1+/g;
+    var param = str;
+    var answer = permute(param);
+
 
 function permute(input) {
   var i, ch, chars = input.split("");
@@ -17,42 +22,37 @@ function permute(input) {
     usedChars.pop();
   }
  return permArr;
-};
+}
 
-//REGULAR EXPRESSION TO MATCH REPEATING CHARACTER IN A STRING
-const reg = /(\w)\1+/g;
-var param = ("aab");
-var answer = permute(param);
+
 
 //match repeating characters in a string
 //parsestring3= "aab".match(reg);
 function findRepeatingChar(arr){
-	var output=[];
-	for (var i = 0; i < arr.length; i++) {
-		if(arr[i].match(reg)!=null){
-		//console.log("A match!");
-		}
-		else{
-			output.push(arr[i]);
-		}
-	}
-	return output;
+  var output=[];
+  for (var i = 0; i < arr.length; i++) {
+    if(arr[i].match(reg)!=null){
+    //console.log("A match!");
+    }
+    else{
+      output.push(arr[i]);
+    }
+  }
+  return output;
 }
 
 
-console.log(answer);
+//console.log(answer);
 
-console.log(findRepeatingChar(answer))
-console.log(findRepeatingChar(answer).length);
+//console.log(findRepeatingChar(answer))
+return(findRepeatingChar(answer).length);
+}
+
+//cycle through array to create permutations
+permAlone("aab");
 
 
-//calculate factorial function NOT USED
-function fact(num)
-{
-    if(num<0)
-     return "Undefined";
-    var fact=1;
-    for(var i=num;i>1;i--)
-      fact*=i;
-    return fact;
- }
+
+
+
+
