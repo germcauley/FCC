2143e6c11c1d3a48ed553e5fac6aa194a272482c
Gerald McAuley <geraldmcauley@gmail.com>
2018-01-09 12:05:28 +0000

    Add files via upload

--- mapTheDebris.js
+++ mapTheDebris.js
@@ -1,17 +1,27 @@
-function orbitalPeriod(arr) {
+
+ function orbitalPeriod(arr) {
   var GM = 398600.4418;
   var earthRadius = 6367.4447;
-  var avgAlt =0;
+ 
+ 
+  var newArr = [];
 
+//Function that gets the orbital period, replaces it in the object
+  var getOrbPeriod = function(obj) {
+  	var r = earthRadius+arr[elem].avgAlt;
+   	var T =Math.round(2*3.14159*(Math.sqrt((Math.pow(r,3))/GM)));
+    delete obj.avgAlt;
+    obj.orbitalPeriod = T;
+    return obj;
+  };
 
-  var a =0;
-  var b = Math.pow(earthRadius);
-  var c =0;
-
-console.log(arr[0].name);
-
-
-  return arr;
+  //call the orbita period function for every object in the array
+  for (var elem in arr) {
+    newArr.push(getOrbPeriod(arr[elem]));
+  }
+  console.log(newArr);
+  return newArr;
 }
 
-orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]);
+// test here
+orbitalPeriod([{name: "iss", avgAlt: 413.6}, {name: "hubble", avgAlt: 556.7}, {name: "moon", avgAlt: 378632.553}])
