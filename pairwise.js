6ad9263af004f8f34468196624df50151854968c
Gerald McAuley <geraldmcauley@gmail.com>
2018-01-09 15:40:20 +0000

    Add files via upload

--- pairwise.js
+++ pairwise.js
@@ -20,26 +20,25 @@ function getIndices(arr,arg){
 
 			arr[i]+arr[j]==arg ?
 				console.log('match!') +
-				newArr.push([i,j])
+				newArr.push([i,j])	
 				:console.log('');
 		}
 	}
+	for(var x=0; x<arg;x++){
+
+	}
+	var merged = [].concat.apply([], newArr);
+	console.log(merged);
+	var x=0;
+	while(x < merged.length){
+		var answer = merged[x]+=merged[x]+1;
+		x++;
+		console.log('the answer is:'+answer);
+	}
 	console.log(newArr);
+	return(answer);
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-pairwise([1,4,2,3,0,5], 7);
+//pairwise([1,4,2,3,0,5], 7);
+pairwise([0, 0, 0, 0, 1, 1], 1);
